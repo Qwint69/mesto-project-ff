d844801f82516c79b94720a0a91ca562a1382d06
Qwint <qwint69@yandex.ru>
2024-02-08 14:55:41 +0300

    123

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const content = document.querySelector('.content')
+const addButton = content.querySelector('.profile__add-button')
+const placesList = content.querySelector('.places__list')
+
+
+function addCard(name, link) {
+    const cardTemplate = document.querySelector('#card-template').content
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+
+    cardElement.querySelector('.card__title').textContent = name
+    cardElement.querySelector('.card__image').src = link
+
+    placesList.appendChild(cardElement)
+
+    return cardElement
+}
+
+    const cardElements = initialCards.map((card) => addCard(card.name, card.link))
+    cardElements.forEach((element) => {
+        const deleteButton = element.querySelector('.card__delete-button')
+        deleteButton.addEventListener('click', () => element.remove())
+    })
+
+
+
+
+
+
+
+
