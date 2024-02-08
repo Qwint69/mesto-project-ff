6d86dc96cd620230ee2b49337e2a0c9e15e4a271
Qwint <qwint69@yandex.ru>
2024-02-08 21:28:02 +0300

    remarks

--- scripts/index.js
+++ scripts/index.js
@@ -9,27 +9,30 @@
 // @todo: Вывести карточки на страницу
 
 const content = document.querySelector('.content')
-const addButton = content.querySelector('.profile__add-button')
 const placesList = content.querySelector('.places__list')
-
+const cardTemplate = document.querySelector('#card-template').content
 
 function addCard(name, link) {
-    const cardTemplate = document.querySelector('#card-template').content
+    const cardElement = createCard(name, link)
+
+    placesList.appendChild(cardElement)
+}
+
+function createCard(name, link) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
 
     cardElement.querySelector('.card__title').textContent = name
     cardElement.querySelector('.card__image').src = link
-
-    placesList.appendChild(cardElement)
+    cardElement.querySelector('.card__image').alt = name
+    const deleteButton = cardElement.querySelector('.card__delete-button')
+    deleteButton.addEventListener('click', () => cardElement.remove())
 
     return cardElement
 }
 
-    const cardElements = initialCards.map((card) => addCard(card.name, card.link))
-    cardElements.forEach((element) => {
-        const deleteButton = element.querySelector('.card__delete-button')
-        deleteButton.addEventListener('click', () => element.remove())
-    })
+initialCards.forEach((card) => addCard(card.name, card.link))
+
+
 
 
 
