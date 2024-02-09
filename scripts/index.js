33dad92360fba04840847ac261281fb3ed3d4807
Qwint <qwint69@yandex.ru>
2024-02-09 15:06:33 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -13,21 +13,25 @@ const placesList = content.querySelector('.places__list')
 const cardTemplate = document.querySelector('#card-template').content
 
 function addCard(name, link) {
-    const cardElement = createCard(name, link)
+    const cardElement = createCard(name, link, deleteCard)
 
     placesList.appendChild(cardElement)
 }
 
-function createCard(name, link) {
+function createCard(name, link, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
 
     cardElement.querySelector('.card__title').textContent = name
     cardElement.querySelector('.card__image').src = link
     cardElement.querySelector('.card__image').alt = name
-    const deleteButton = cardElement.querySelector('.card__delete-button')
-    deleteButton.addEventListener('click', () => cardElement.remove())
+    const buttonDeleteButton = cardElement.querySelector('.card__delete-button')
+    buttonDeleteButton.addEventListener('click', () => deleteCard(cardElement))
 
     return cardElement
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove()
 }
 
 initialCards.forEach((card) => addCard(card.name, card.link))
